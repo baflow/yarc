2227076198005d3ee4a34937aa59ad2df37858fe
Greg <rapatatui@gmail.com>
2017-04-14 16:34:29 +0200

    threading added

--- main.py
+++ main.py
@@ -18,26 +18,22 @@ class Worker(QtCore.QThread):
         self.howDeep = 1
         self.mimeType = []
         self.mimeTypeSort = []
+        self.imgExt = []
         self.filesNum = 0
-
         self.headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
         }
 
-
-
     def run(self):
-
         self.howManyTimes = MainWindow.howManyPages.value()
         self.url = ("reddit.com/r/" + MainWindow.comboBox.currentText())
+
         self.r = requests.get('http://' + self.url, headers=self.headers)
         self.soup = BeautifulSoup(self.r.content, 'lxml')
-
         if self.soup.find('img', {'class':'interstitial-image'}):
             self.r = requests.post('https://www.reddit.com/over18?dest=https://www.' + self.url ,data={'over18':'yes'},headers=self.headers)
             self.soup = BeautifulSoup(self.r.content, 'lxml')
             self.get_doc()
-
         else:
             self.get_doc()
 
@@ -46,7 +42,6 @@ class Worker(QtCore.QThread):
         # Regular expression for links in page source
         self.links = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
                                 self.soup.prettify())
-
         # chceck image type and append tuple to list
         for link in self.links:
             self.mimeType.append(mimetypes.guess_type(link))
@@ -60,49 +55,38 @@ class Worker(QtCore.QThread):
                 self.mimeTypeSort.append('GIF')
             else:
                 self.mimeTypeSort.append(i[0])
-
         # merge lists into dict
         self.linkMimeDict = dict(zip(self.links, self.mimeTypeSort))
         # Get rid of garbage aka None
         self.linkMimeDict = {k: v for k, v in self.linkMimeDict.items() if v}
-        print self.linkMimeDict
-
-
         self.saveFile()
 
     def saveFile(self):
 
         self.today = str(datetime.now())
-
         if not os.path.exists('img/' + self.today[:10] + '/' + MainWindow.comboBox.currentText()):
             os.makedirs('img/' + self.today[:10] + '/' + MainWindow.comboBox.currentText())
 
-        for link in self.linkMimeDict:
+        for link,ext in self.linkMimeDict.items():
             try:
                 self.file_numb = random.randint(0000000000, 2494849328)
                 self.r = requests.get(link)
                 self.i = Image.open(BytesIO(self.r.content), 'r')
-
-
                 self.fileName = 'img/' + self.today[:10] + '/' + MainWindow.comboBox.currentText()+ '/' + str(
-                    self.file_numb) + '.' + 'jpg'
-
+                    self.file_numb) + '.' + ext
                 self.i.save(self.fileName)
             except:
-                print('wrong input type')
+                print('error')
                 pass
 
         # go to next page if needed
         if self.howManyTimes > 1:
             while self.howDeep <= self.howManyTimes:
                 self.url = self.soup.find('a', {'rel': 'nofollow next'}, href=True)
-
                 self.r = requests.get(self.url['href'], headers=self.headers)
                 self.howDeep += 1
                 self.soup = BeautifulSoup(self.r.content, 'lxml')
-
                 self.get_doc()
-
 
 class MainWindow(QtWidgets.QMainWindow, ui_yarc.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -110,17 +94,16 @@ class MainWindow(QtWidgets.QMainWindow, ui_yarc.Ui_MainWindow):
         self.setupUi(self)
         self.myThread = Worker()
         self.cb = QtWidgets.QComboBox
+        self.label_wrk.hide()
         self.getBut.clicked.connect(self.myThread.start)
-
-        self.stopBut.clicked.connect(self.myThread.terminate)
         self.getBut.clicked.connect(self.cb)
-
-
-
+        self.stopBut.clicked.connect(self.myThread.terminate)
+        self.stopBut.clicked.connect(self.label_wrk.hide)
+        self.myThread.started.connect(self.label_wrk.show)
+        self.myThread.finished.connect(self.label_wrk.hide)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-
     MainWindow = MainWindow()
     MainWindow.show()
     app.exec_()
